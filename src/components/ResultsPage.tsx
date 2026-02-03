55f9acfeb1b18b5f79c4323c7969e97219e98e9a
Atom-SA <ia@atompar.com.br>
2026-02-03 09:35:19 -0300

    Updated ResultsPage.tsx

--- src/components/ResultsPage.tsx
+++ src/components/ResultsPage.tsx
@@ -18,7 +18,7 @@ export default function ResultsPage({ formData, finalPrice }: ResultsPageProps) 
     <div className="min-h-screen bg-[#003366] font-inter">
       <div className="fixed top-0 left-0 right-0 bg-[#003366] z-50">
         <div className="max-w-6xl mx-auto px-6 py-4">
-          <Logo invert height="h-5" />
+          <Logo invert height="h-5" brightness="10" />
         </div>
       </div>
 
